91c8bd5f40ac91589f3231a9a92b514a15c18753
purushottam <purushottam@rhythmflows.com>
2023-06-27 12:10:39 +0530

    commit

--- backend/taxapp/models.py
+++ backend/taxapp/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+class Country(models.Model):
+    country=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here
 class Tax(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
+    country = models.CharField(max_length=20)
+    # country = models.ForeignKey(Country,on_delete=models.CASCADE)
     taxtype = models.CharField(max_length=10)
     zone=models.CharField(max_length=255, null=True, blank=True)
     ward=models.CharField(max_length=255, null=True, blank=True)
